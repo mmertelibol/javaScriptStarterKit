c0509701167a61d9a8e46827983121f2e8b24a71
Mert <m.elibol@bilgeadam.com>
2021-10-19 00:04:16 +0300

    firstDemo18.10

---
+++ src/app.js
@@ -0,0 +1,27 @@
+console.log("selam")
+
+
+let dolarBugun= 9.30;
+let dolarDun= 9.20;
+ dolarDun=9.10;
+
+{
+  let dolarDun=9.50
+
+}
+
+console.log(dolarDun)
+const euroDun=11.0
+//euroDun=11.2
+console.log(euroDun)
+
+//camelCasing
+//PascalCasing
+let konutKredileri=["Konut Kredisi", "Araç Kredisi","İhtiyaç Kredisi"];
+for (let index = 0; index < konutKredileri.length; index++) {
+    const element = konutKredileri[index];
+    console.log(element)
+    
+}
+//let users= getUsersFromApi()
+console.log(konutKredileri)
